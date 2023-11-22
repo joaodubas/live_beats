a3c8d641d9b3bcdcf145820082eb494a1fe128aa
Joao P Dubas <joao.dubas@gmail.com>
2023-11-22 08:25:10 -0300

    chore: database config from env vars in dev/test

--- config/test.exs
+++ config/test.exs
@@ -11,10 +11,10 @@ config :live_beats, :files,
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :live_beats, LiveBeats.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "live_beats_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  hostname: System.get_env("POSTGRES_HOSTNAME", "localhost"),
+  database: System.get_env("POSTGRES_DATABASE", "live_beats_test#{System.get_env("MIX_TEST_PARTITION")}"),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
